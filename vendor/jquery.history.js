a3bab82ebe5e539d2f5eccffc72850242a2cab60
Marcello Barnaba <vjt@openssl.it>
2010-02-17 12:15:46 +0100

    AJAX History: whitespace

--- vendor/jquery.history.js
+++ vendor/jquery.history.js
@@ -13,109 +13,109 @@
 
 (function ($) {
 
-	var _current, _callback;
+  var _current, _callback;
 
-	var iframe;
+  var iframe;
 
-	function iframe_init(hash) {
-		iframe = $('#ie_history')[0];
-	}
+  function iframe_init(hash) {
+    iframe = $('#ie_history')[0];
+  }
 
-	function iframe_set(hash) {
-		try {
-			var doc = iframe.contentWindow.document;
+  function iframe_set(hash) {
+    try {
+      var doc = iframe.contentWindow.document;
 
-			doc.open();
-			doc.write('<html><body>' + hash + '</body></html>');
-			doc.close();
+      doc.open();
+      doc.write('<html><body>' + hash + '</body></html>');
+      doc.close();
 
-			return true;
-		} catch (e) {
-			return false;
-		}
-	}
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
 
-	function iframe_get() {
-		try {
-			return iframe.contentWindow.document.body.innerText;
-		} catch (e) { return ''; }
-	}
+  function iframe_get() {
+    try {
+      return iframe.contentWindow.document.body.innerText;
+    } catch (e) { return ''; }
+  }
 
-	function invoke () {
-		if (_current)
-			_callback (_current.replace(/^#/, ''));
-	}
+  function invoke () {
+    if (_current)
+      _callback (_current.replace(/^#/, ''));
+  }
 
-	function stripQuery(s) {
-		return s.replace(/\?.*$/, '');
-	}
+  function stripQuery(s) {
+    return s.replace(/\?.*$/, '');
+  }
 
-	function changed(hash) {
-		return (hash && hash != 'false' && hash != _current);
-	}
+  function changed(hash) {
+    return (hash && hash != 'false' && hash != _current);
+  }
 
 
-	function monitor () {
-		var hash;
+  function monitor () {
+    var hash;
 
-		if (iframe)
-			hash = stripQuery (iframe_get ());
-		else
-			hash = stripQuery (location.hash);
+    if (iframe)
+      hash = stripQuery (iframe_get ());
+    else
+      hash = stripQuery (location.hash);
 
-		if (!changed (hash))
-			return;
+    if (!changed (hash))
+      return;
 
-		$.history.save (hash, true);
-		invoke ();
-	}
+    $.history.save (hash, true);
+    invoke ();
+  }
 
-	$.history = {
-		init: function (callback) {
-			_callback = callback;
+  $.history = {
+    init: function (callback) {
+      _callback = callback;
 
-			var hash = stripQuery (location.hash);
-			if (hash == '')
-				hash = '#';
+      var hash = stripQuery (location.hash);
+      if (hash == '')
+        hash = '#';
 
-			if ($.browser.msie && ($.browser.version < 8 || document.documentMode < 8))
-				iframe_init (hash);
+      if ($.browser.msie && ($.browser.version < 8 || document.documentMode < 8))
+        iframe_init (hash);
 
-			else if ($.browser.opera)
-				history.navigationMode = 'compatible';
+      else if ($.browser.opera)
+        history.navigationMode = 'compatible';
 
-			setInterval (monitor, 100);
-		},
+      setInterval (monitor, 100);
+    },
 
-		load: function (hash) {
-			if (!changed (hash))
-				return;
+    load: function (hash) {
+      if (!changed (hash))
+        return;
 
-			$.history.save (hash);
-			invoke ();
-		},
+      $.history.save (hash);
+      invoke ();
+    },
 
-		save: function (hash, skipIframe) {
-			hash = decodeURIComponent (stripQuery (hash))
+    save: function (hash, skipIframe) {
+      hash = decodeURIComponent (stripQuery (hash))
 
-			if (!hash.match (/^#/))
-				hash = '#' + hash;
+      if (!hash.match (/^#/))
+        hash = '#' + hash;
 
-			if (!changed (hash)) {
-				return;
-			}
+      if (!changed (hash)) {
+        return;
+      }
 
-			_current = hash;
+      _current = hash;
 
-			location.hash = hash;
+      location.hash = hash;
 
-			if (iframe && !skipIframe)
-				iframe_set (hash);
-		},
+      if (iframe && !skipIframe)
+        iframe_set (hash);
+    },
 
-		current: function () {
-			return _current;
-		}
-	};
+    current: function () {
+      return _current;
+    }
+  };
 
 })(jQuery);
