bcf47903976879c39857ea30a9d01a9f0c8e079b
Fabrizio Regini <freegenie@gmail.com>
2010-06-06 00:13:04 +0800

    better demo
    Styles and 202 response

--- config.ru
+++ config.ru
@@ -2,10 +2,8 @@ require 'logger'
 require 'rubygems'
 require 'rack'
 require 'demo/demo' 
-require 'pp'
 
 use Rack::Reloader
-
 
 map "/" do 
   run Demo.new
--- demo/demo.rb
+++ demo/demo.rb
@@ -1,10 +1,11 @@
 class Demo
 
   def call(env)
-    # pp env
     # return index.html when pointing to root
     if env['REQUEST_PATH'] == '/'
       [200, {"Content-Type" => "text/html"}, [File.read(File.join(File.dirname(__FILE__), 'index.html'))]]
+    elsif env['REQUEST_PATH'] == '/form'    
+      [202, {"Content-Type" => "text/html"}, ['/form_response.html'] ]
     else
       # try to find the file in the demo path
       file_path = File.join(File.dirname(__FILE__), env['REQUEST_PATH'])
@@ -15,7 +16,7 @@ class Demo
       if File.exists?(file_path)
         [200, {"Content-Type" => "text/html"}, [ File.read(file_path) ]]
       else
-        [404]
+        [404, {"Content-Type" => "text/html"}, [] ]
       end
     end
   end
