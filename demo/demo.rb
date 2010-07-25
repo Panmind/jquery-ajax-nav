7182142c88955f50bdb88c9766b05cfbc758a24b
Marcello Barnaba <vjt@openssl.it>
2010-07-25 16:18:54 +0200

    Demo: rewrote, DRYed, cleaned up, commented

--- demo/demo.rb
+++ demo/demo.rb
@@ -1,24 +1,49 @@
+require 'pathname'
+require 'rack/mime'
+require 'rack/request'
+
 class Demo
 
   def call(env)
-    # return index.html when pointing to root
-    if env['REQUEST_PATH'] == '/'
-      [200, {"Content-Type" => "text/html"}, [File.read(File.join(File.dirname(__FILE__), 'index.html'))]]
-    elsif env['REQUEST_PATH'] == '/form'    
-      [202, {"Content-Type" => "text/html"}, ['/form_response.html'] ]
+    req = Rack::Request.new(env)
+
+    case req.path
+    when '/'
+      # If the request comes via XHR, don't return the whole index.html page
+      #
+      name = 'index.html'
+      [200, {'Content-Type' => 'text/html'}, [ asset(name).read ]]
+
+    when '/form'
+      # Return a 202 on this path, to demonstrate AJAX redirects: the body
+      # of the request must contain the path where the client side JS will
+      # redirect to.
+      #
+      [202, {'Content-Type' => 'text/plain'}, [ '/form_response.html' ]]
+
     else
-      # try to find the file in the demo path
-      file_path = File.join(File.dirname(__FILE__), env['REQUEST_PATH'])
-      unless File.exists?(file_path)
-        # try to find the file in upper path (maybe we are asking for js)
-        file_path = File.join(File.dirname(__FILE__), '..', env['REQUEST_PATH'])
-      end
-      if File.exists?(file_path)
-        [200, {"Content-Type" => "text/html"}, [ File.read(file_path) ]]
+      # Try to find the file in the demo path and serve it
+      #
+      asset = asset(req.path)
+
+      if asset
+        [200, {'Content-Type' => Rack::Mime.mime_type(asset.extname)}, [ asset.read ]]
       else
-        [404, {"Content-Type" => "text/html"}, [] ]
+        [404, {'Content-Type' => 'text/plain'}, [ 'not found' ]]
       end
     end
   end
+
+  private
+    Root = Pathname('.').realpath
+
+    # Returns a pathname for the given asset name by removing an
+    # eventual leading / and joining it to the demo Root. If the
+    # result is not a file or is not readable, \nil is returned.
+    #
+    def asset(name)
+      path = Root.join name.to_s.sub(/^\//, '')
+      path.file? && path.readable? ? path : nil
+    end
 
 end
